f081492a8aa39ee4dc1cffe2d2e9b75023a9e25a
theo <tkleinen@gmail.com>
2017-08-30 09:53:16 +0200

    adds more fields to output

--- pzh/management/commands/summary.py
+++ pzh/management/commands/summary.py
@@ -18,10 +18,13 @@ class Command(BaseCommand):
     help = 'overzicht loggers en files'
                         
     def handle(self, *args, **options):
-        print 'name, nitg, filter, code, refpnt, top, bottom, logger, refpnt, depth, baro, file, begin, end, days, points'
+        print 'name, nitg, filter, code, maaiveld, ahn3, bovenkantbuis, top, bottom, logger, ophangpunt, kabellengte, baro, aantal, monfile, monbegin, moneinde, monaantal'
         for w in Well.objects.all():
             baro = w.meteo.baro if hasattr(w,'meteo') else None
             for s in w.screen_set.all():
+                series = s.mloc.series_set.filter(name__endswith='COMP').first()
                 for lp in s.loggerpos_set.all():
+                    #lppts = series.datapoints.filter(date__gte=lp.start, date__lte=lp.stop).count() if series else 0
                     for mf in lp.monfile_set.all():
-                        print ','.join([str(x) for x in [w.name, w.nitg, s.nr, unicode(s), s.refpnt, s.top, s.bottom, lp.logger, lp.refpnt, lp.depth, q(baro), q(mf.name), d(mf.start_date), d(mf.end_date), (mf.end_date-mf.start_date).days, mf.num_points]])
+                        mfpts = series.datapoints.filter(date__gte=mf.start_date, date__lte=mf.end_date).count() if series else 0
+                        print ','.join([str(x) for x in [w.name, w.nitg, s.nr, unicode(s), w.maaiveld, w.ahn, s.refpnt, s.top, s.bottom, lp.logger, lp.refpnt, lp.depth, q(baro), mfpts, q(mf.name), d(mf.start_date), d(mf.end_date), mf.num_points]])
