0941b37353ac10be88819b81cce693ce08fdc393
theo <tkleinen@gmail.com>
2018-09-30 15:41:10 +0200

    makecharts management command takes begin and end year as optional arguments

--- pzh/management/commands/makecharts.py
+++ pzh/management/commands/makecharts.py
@@ -8,6 +8,8 @@ from acacia.meetnet.models import Well
 from acacia.meetnet.util import chart_for_well, chart_for_screen
 import os
 from acacia.data.util import slugify
+import pytz
+import datetime
 
 class Command(BaseCommand):
     help = 'maak grafiekjes'
@@ -23,24 +25,38 @@ class Command(BaseCommand):
                 dest = 'noscreen',
                 default = False,
                 help = 'ignore screens')
+
+        parser.add_argument('-b', '--begin',
+                action='store',
+                dest = 'begin',
+                default = False,
+                help = 'first year')
+
+        parser.add_argument('-e', '--end',
+                action='store',
+                dest = 'end',
+                default = False,
+                help = 'last year')
                         
     def handle(self, *args, **options):
         folder = options.get('dest')
         noscreen = options.get('noscreen')
-        #tz = pytz.timezone('CET')
-        #start=datetime(2013,1,1,tzinfo=tz)
-        #stop=datetime(2016,12,31,tzinfo=tz)
+        begin = options.get('begin',2013)
+        end = options.get('end',2017)
+        tz = pytz.timezone('CET')
+        start=datetime(int(begin),1,1,tzinfo=tz)
+        stop=datetime(int(end),12,31,tzinfo=tz)
         if not os.path.exists(folder):
             os.makedirs(folder)
         for w in Well.objects.all():
-            data = chart_for_well(w)
+            data = chart_for_well(w,start=start,stop=stop)
             filename = os.path.join(folder,slugify(unicode(w)) + '.png')
             print filename
             with open(filename,'wb') as png:
                 png.write(data)
             if not noscreen:
                 for s in w.screen_set.all():
-                    data = chart_for_screen(s)
+                    data = chart_for_screen(s,start=start,stop=stop)
                     filename = os.path.join(folder,slugify(unicode(s)) + '.png')
                     print filename
                     with open(filename,'wb') as png:
