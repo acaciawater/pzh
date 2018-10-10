2e4647572805bd6f81202eca1a94d8892a269ff8
theo <tkleinen@gmail.com>
2018-10-10 10:40:38 +0200

    timeline accepts variable time range

--- pzh/management/commands/timeline.py
+++ pzh/management/commands/timeline.py
@@ -6,17 +6,29 @@ Created on Jul 20, 2017
 from django.core.management.base import BaseCommand
 from acacia.meetnet.models import Screen
 import datetime
-from acacia.mqtt.models import start
 
 class Command(BaseCommand):
     args = ''
     help = 'maak timeline van metingen'
                         
+    def add_arguments(self, parser):
+        parser.add_argument('-b', '--begin',
+                action='store',
+                dest = 'begin',
+                default = 2017,
+                help = 'first year')
+
+        parser.add_argument('-e', '--end',
+                action='store',
+                dest = 'end',
+                default = 2017,
+                help = 'last year')
+
     def handle(self, *args, **options):
-        first = datetime.date(2013,1,1)
-        last = datetime.date(2016,12,31)
+        first = datetime.date(int(options.get('begin')),1,1)
+        last = datetime.date(int(options.get('end')),12,31)
         for s in Screen.objects.order_by('well','nr'):
-            query = s.loggerpos_set.order_by('start_date')
+            query = s.loggerpos_set.filter(end_date__gt=first,start_date__lt=last).order_by('start_date')
             empty = True
             for lp in query:
                 start = lp.start_date.date()
@@ -35,7 +47,9 @@ class Command(BaseCommand):
                         print unicode(s), d2, start, 'ontbreekt'
                         d1 = start
                         d2 = end
-            if not empty:
+            if empty:
+                print unicode(s), first, last, 'ontbreekt'
+            else:
                 if d1 != start:
                     print unicode(s), d1, d2, 'aanwezig'
                 if d2 < last:
