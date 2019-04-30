8704aa1e4b4759bc835c1b9e0a63a614e4276931
theo <tkleinen@gmail.com>
2019-04-30 16:20:08 +0200

    ManualSeries always GMT-1

--- pzh/management/commands/hand.py
+++ pzh/management/commands/hand.py
@@ -4,25 +4,29 @@ Created on Dec 6, 2014
 @author: theo
 '''
 import csv, datetime
-from django.core.management.base import BaseCommand
-from acacia.data.models import MeetLocatie, ManualSeries
-from acacia.meetnet.models import Well,Screen
-from django.db.models import Q
+
 from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+from django.db.models import Q
 import pytz
+
+from acacia.data.models import ProjectLocatie, MeetLocatie, ManualSeries
+from acacia.meetnet.models import Well, Screen
 
 class Command(BaseCommand):
     args = ''
     help = 'Importeer handpeilingen'
-
     def add_arguments(self, parser):
-        parser.add_argument(dest='fname',
-                help='Name of csv file with water level readings')
-        
+        parser.add_argument('-f','--file',
+                action='store',
+                dest = 'fname',
+                default = None,
+                help='CSV file met handpeilingen'
+        )
         
     def handle(self, *args, **options):
         fname = options.get('fname')
-        TZ=pytz.timezone('CET') # handpeilingen worden aangeleved in CET
+        CET=pytz.timezone('Etc/GMT-1')
         user=User.objects.get(username='theo')
         if fname:
             with open(fname,'r') as f:
@@ -33,26 +37,31 @@ class Command(BaseCommand):
                         well = Well.objects.get(Q(nitg=NITG) | Q(name=NITG))
                         filt = int(row['Filter'])
                         screen = well.screen_set.get(nr=filt)
-                        mloc = screen.mloc
+                        #ploc = ProjectLocatie.objects.get(name=well.nitg)
+                        ploc = ProjectLocatie.objects.get(name__in=[well.nitg, well.name])
+                        #name1= '%s/%03d' % (well.name, filt)
+                        name2= '%s/%03d' % (well.nitg, filt)
+                        mloc = ploc.meetlocatie_set.get(name=name2)
                         datumtijd = '%s %s' % (row['Datum'], row['Tijd'])
                         depth = row['Meting']
                         if depth:
                             depth = float(depth)
                         else:
-                            depth = 0
+                            continue
                         if not screen.refpnt:
                             print 'Reference point for screen %s not available' % screen
                             continue
                         nap = screen.refpnt - depth
                         date = datetime.datetime.strptime(datumtijd,'%d/%m/%Y %H:%M')
-                        date = TZ.localize(date)
+                        date = CET.localize(date)
                         series_name = '%s HAND' % mloc.name
-                        series, created = ManualSeries.objects.get_or_create(name=series_name, mlocatie=mloc, defaults = {'description': 'Handpeiling', 'unit': 'm NAP', 'type': 'scatter', 'user':user})
-                        pt, created = series.datapoints.get_or_create(date=date,defaults={'value': nap})
-                        if not created:
-                            pt.value = nap
-                            pt.save()
-                        print screen, pt.date, pt.value
+                        series,created = ManualSeries.objects.get_or_create(name=series_name,mlocatie=mloc,defaults={'description':'Handpeiling', 'timezone':'Etc/GMT-1', 'unit':'m NAP', 'type':'scatter', 'user':user})
+                        if row.get('Verwijderen','nee') == 'ja':
+                            deleted = series.datapoints.filter(date=date).delete()
+                            print screen, date, ('deleted' if deleted else 'NOT deleted')
+                        else:
+                            pt, created = series.datapoints.update_or_create(date=date,defaults={'value': nap})
+                            print screen, pt.date, pt.value
                     except Well.DoesNotExist:
                         print 'Well %s not found' % NITG
                     except Screen.DoesNotExist:
