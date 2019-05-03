9907dc57f02578cbdf2960eaebbe5f9f3b8b7ebf
theo <tkleinen@gmail.com>
2019-05-03 10:26:23 +0200

    check_hand: print begin and end on one line

--- pzh/management/commands/check_hand.py
+++ pzh/management/commands/check_hand.py
@@ -3,10 +3,12 @@ from django.core.management.base import BaseCommand
 from acacia.data.models import ManualSeries, DataPoint
 from acacia.meetnet.models import Screen
 from datetime import timedelta
+import pytz
 
 class Command(BaseCommand):
     args = ''
-    help = 'Check afwijkende handpeilingen'
+    help = 'Check afwijkende handpeilingen aan begin/einde meetronde'
+    tz = pytz.timezone('Etc/GMT-1')
 
     def find_nearest(self, queryset, date):
         points = queryset.order_by('date')
@@ -40,14 +42,14 @@ class Command(BaseCommand):
         return (peiling,level) 
 
     def _toString(self,dp):
-        return ','.join(map(str,[dp.date, dp.value])) if dp else ','
+        return ','.join([dp.date.astimezone(self.tz).strftime('%Y-%m-%d %H:%M'), str(dp.value)]) if dp else ','
 
     def toString(self,dp):
         return ','.join([self._toString(p) for p in dp])
 
     def handle(self, *args, **options):
         tolerance = timedelta(hours=2)
-        print 'screen,monfile,logger,hand.date,hand.value,logger.date,logger.value'
+        print 'screen,monfile,logger,start.hand.date,start.hand.value,start.logger.date,start.logger.value,stop.hand.date,stop.hand.value,stop.logger.date,stop.logger.value'
         for screen in Screen.objects.all():
             series = screen.find_series()
             peilingen = screen.mloc.series_set.instance_of(ManualSeries).filter(name__endswith='HAND').first()
@@ -55,6 +57,5 @@ class Command(BaseCommand):
                 for mon in screen.get_monfiles():
                     start=self.process(peilingen, series, mon.start_date, tolerance)
                     stop=self.process(peilingen, series, mon.end_date, tolerance)
-                    print ','.join([str(screen),mon.name,mon.serial_number,self.toString(start)])
-                    print ','.join([str(screen),mon.name,mon.serial_number,self.toString(stop)])
+                    print ','.join([str(screen),mon.name,mon.serial_number,self.toString(start),self.toString(stop)])
                     
