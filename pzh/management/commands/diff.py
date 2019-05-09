780657aa3c291e107b4acd034aa1bfea972182fd
theo <tkleinen@gmail.com>
2019-05-09 22:45:22 +0200

    1 hour tolerance for comparisions

--- pzh/management/commands/diff.py
+++ pzh/management/commands/diff.py
@@ -13,6 +13,34 @@ class Command(BaseCommand):
     help = 'Vergelijk standen en handpeilingen'
 
     def handle(self, *args, **options):
+
+        tolerance = pd.Timedelta('1 hour')
+        print 'tolerance:',tolerance
+        
+        header = True
+        with open('diff.csv','w') as csv:
+            for screen in Screen.objects.all():
+                print unicode(screen),
+                hand = screen.get_manual_series()
+                if hand is None:
+                    print 'geen handpeilingen'
+                else:
+                    src = screen.get_compensated_series()
+                    if src is None:
+                        print 'geen loggerdata'
+                    else:
+                        try:
+                            data = src.reindex(hand.index,method='nearest',tolerance=tolerance)
+                            verschil = data - hand
+                            df = pd.DataFrame({'filter': unicode(screen), 'data': data, 'hand': hand, 'verschil': verschil})
+                            df.dropna(inplace=True)
+                            print verschil.mean()
+                            df.to_csv(csv,header=header)
+                            header = False
+                        except Exception as e:
+                            print 'problem with',screen,e
+
+    def handle2(self, *args, **options):
         with open('diff.csv','w') as csv:
             for screen in Screen.objects.all():
                 print unicode(screen),
