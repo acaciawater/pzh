97856bfbef9465f5e887116f60138d517c4f009f
theo <tkleinen@gmail.com>
2019-06-19 15:19:23 +0200

    Disable debug toolbar

--- pzh/settings.py
+++ pzh/settings.py
@@ -22,7 +22,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['.acaciadata.com','localhost']
 
 # for debug toolbar
-INTERNAL_IPS = '127.0.0.1'
+#INTERNAL_IPS = '127.0.0.1'
 
 SITE_ID = 1
 
@@ -85,18 +85,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pzh.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
